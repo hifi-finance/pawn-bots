734e91512e3389d33cfe02fba10f2f6e21a224a1
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2021-12-12 00:40:22 +0300

    feat: add basic BFF interface feat: add basic BFF implementation docs: add license docs: update package info

---
+++ contracts/BotFarmFrens.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "hardhat/console.sol";
+import "./IBotFarmFrens.sol";
+
+error BotFarmFrens__MaxBffsMintOverflow();
+error BotFarmFrens__MaxBffsSupplyOverflow();
+
+/// @title BotFarmFrens
+/// @author Hifi
+/// @notice Manages the mint and distribution of BFFs.
+contract BotFarmFrens is IBotFarmFrens, ERC721Enumerable, Ownable {
+    /// PUBLIC STORAGE ///
+
+    /// @inheritdoc IBotFarmFrens
+    IERC20Metadata public override currency;
+
+    /// @inheritdoc IBotFarmFrens
+    uint256 public override price;
+
+    /// INTERNAL STORAGE ///
+
+    /// @dev The maximum possible number of BFFs that can be minted in one transaction.
+    uint256 internal constant MAX_BFFS_MINT = 5;
+
+    /// @dev The maximum possible number of BFFs in the collection.
+    uint256 internal constant MAX_BFFS_SUPPLY = 10_000;
+
+    constructor(IERC20Metadata currency_) ERC721("Bot Farm Frens", "BFF") {
+        // solhint-disable-previous-line no-empty-blocks
+        currency = currency_;
+        price = 100 * 10**currency.decimals();
+    }
+
+    /// PUBLIC NON-CONSTANT FUNCTIONS ///
+
+    /// @inheritdoc IBotFarmFrens
+    function mintBff(uint256 mintAmount) public override {
+        if (mintAmount > MAX_BFFS_MINT) {
+            revert BotFarmFrens__MaxBffsMintOverflow();
+        }
+        if (mintAmount + totalSupply() > MAX_BFFS_SUPPLY) {
+            revert BotFarmFrens__MaxBffsSupplyOverflow();
+        }
+        currency.transferFrom(msg.sender, address(this), price * mintAmount);
+        for (uint256 i = 0; i < mintAmount; i++) {
+            _safeMint(msg.sender, totalSupply());
+        }
+    }
+
+    /// @inheritdoc IBotFarmFrens
+    function setPrice(uint256 newPrice) public override onlyOwner {
+        uint256 oldPrice = price;
+        price = newPrice;
+        emit SetPrice(oldPrice, price);
+    }
+
+    /// @inheritdoc IBotFarmFrens
+    function withdraw() public override onlyOwner {
+        uint256 amount = currency.balanceOf(address(this));
+        currency.transfer(msg.sender, amount);
+        emit Withdraw(amount);
+    }
+}
---
+++ contracts/IBotFarmFrens.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+/// @title IBotFarmFrens
+/// @author Hifi
+interface IBotFarmFrens {
+    /// EVENTS ///
+
+    /// @notice Emitted when mint price is set.
+    /// @param oldPrice The old mint price.
+    /// @param newPrice The new mint price.
+    event SetPrice(uint256 oldPrice, uint256 newPrice);
+
+    /// @notice Emitted when minting BFFs.
+    /// @param amount The amount of BFFs minted.
+    /// @param payment The total currency tokens paid for minting BFFs.
+    event MintBff(uint256 amount, uint256 payment);
+
+    /// @notice Emitted when withdrawing currency.
+    /// @param amount The amount of currency withdrawn.
+    event Withdraw(uint256 amount);
+
+    /// PUBLIC CONSTANT FUNCTIONS ///
+
+    /// @notice The mint payment token contract.
+    function currency() external view returns (IERC20Metadata);
+
+    /// @notice The mint price in currency tokens.
+    function price() external view returns (uint256);
+
+    /// PUBLIC NON-CONSTANT FUNCTIONS ///
+
+    /// @notice Mint new BFFs in exchange for currency.
+    ///
+    /// @dev Emits a {MintBff} event.
+    ///
+    /// Requirements:
+    /// - The caller must have allowed this contract to spend currency tokens.
+    /// - The caller must have `price * mintAmount` currency tokens in their account.
+    ///
+    /// @param mintAmount The amount of BFFs to mint.
+    function mintBff(uint256 mintAmount) external;
+
+    /// @notice Set the mint price.
+    ///
+    /// @dev Emits a {SetPrice} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param newPrice The new BFF mint price.
+    function setPrice(uint256 newPrice) external;
+
+    /// @notice Withdraw the currency contained in the contract.
+    ///
+    /// @dev Emits a {Withdraw} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    function withdraw() external;
+}
