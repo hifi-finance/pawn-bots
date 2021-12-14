63f7501443a8b39cd95f8efd2419b912c99c141b
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2021-12-15 01:54:37 +0300

    feat: implement private sale whitelist feat: implement sale start and pause refactor: rename `MaxBffsMintExceeded` and `MaxBffsSupplyExceeded` errors refactor: rename `MAX_BFFS_SUPPLY` and `MAX_BFFS_MINT` feat: add `maxPublicMints` and `setMaxPublicMints(...)` feat: integrate private and public sale phases into minting

--- contracts/BotFarmFrens.sol
+++ contracts/BotFarmFrens.sol
@@ -7,14 +7,26 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "hardhat/console.sol";
 import "./IBotFarmFrens.sol";
 
-error BotFarmFrens__MaxBffsMintExceeded();
-error BotFarmFrens__MaxBffsSupplyExceeded();
+error BotFarmFrens__EligibilityExceeded();
 error BotFarmFrens__InsufficientCurrency();
+error BotFarmFrens__MaxElementsExceeded();
+error BotFarmFrens__MaxPublicMintsExceeded();
+error BotFarmFrens__NotWhitelisted();
+error BotFarmFrens__SaleAlreadyStarted();
+error BotFarmFrens__SaleIsDisabled();
 
 /// @title BotFarmFrens
 /// @author Hifi
 /// @notice Manages the mint and distribution of BFFs.
 contract BotFarmFrens is IBotFarmFrens, ERC721Enumerable, Ownable {
+    /// STRUCTS ///
+
+    struct WhitelistElement {
+        bool exists;
+        uint256 eligibleAmount;
+        uint256 claimedAmount;
+    }
+
     /// PUBLIC STORAGE ///
 
     /// @inheritdoc IBotFarmFrens
@@ -24,37 +36,61 @@ contract BotFarmFrens is IBotFarmFrens, ERC721Enumerable, Ownable {
     IERC20Metadata public override currency;
 
     /// @inheritdoc IBotFarmFrens
+    uint256 public override maxPublicMints;
+
+    /// @inheritdoc IBotFarmFrens
     uint256 public override price;
+
+    /// @inheritdoc IBotFarmFrens
+    uint256 public override saleStartTime;
+
+    /// @inheritdoc IBotFarmFrens
+    bool public override saleEnabled;
+
+    /// @inheritdoc IBotFarmFrens
+    mapping(address => WhitelistElement) public override whitelist;
+
+    /// @dev The maximum possible number of BFFs in the collection.
+    uint256 public constant MAX_ELEMENTS = 10_000;
+
+    /// @dev The private sale duration from the sale start timestamp.
+    uint256 public constant PRIVATE_SALE_DURATION = 1 days;
 
     /// INTERNAL STORAGE ///
 
-    /// @dev The maximum possible number of BFFs that can be minted in one transaction.
-    uint256 internal constant MAX_BFFS_MINT = 5;
-
-    /// @dev The maximum possible number of BFFs in the collection.
-    uint256 internal constant MAX_BFFS_SUPPLY = 10_000;
-
     constructor(IERC20Metadata currency_) ERC721("Bot Farm Frens", "BFF") {
-        // solhint-disable-previous-line no-empty-blocks
         currency = currency_;
-        price = 100 * 10**currency.decimals();
     }
 
     /// PUBLIC NON-CONSTANT FUNCTIONS ///
 
     /// @inheritdoc IBotFarmFrens
     function mintBFF(uint256 mintAmount) public override {
-        if (mintAmount > MAX_BFFS_MINT) {
-            revert BotFarmFrens__MaxBffsMintExceeded();
+        if (!saleEnabled) {
+            revert BotFarmFrens__SaleIsDisabled();
         }
-        if (mintAmount + totalSupply() > MAX_BFFS_SUPPLY) {
-            revert BotFarmFrens__MaxBffsSupplyExceeded();
+        if (mintAmount + totalSupply() > MAX_ELEMENTS) {
+            revert BotFarmFrens__MaxElementsExceeded();
         }
+        if (block.timestamp <= saleStartTime + PRIVATE_SALE_DURATION) {
+            // private phase
+            if (!whitelist[msg.sender].exists) {
+                revert BotFarmFrens__NotWhitelisted();
+            }
+            if (mintAmount > whitelist[msg.sender].eligibleAmount - whitelist[msg.sender].claimedAmount) {
+                revert BotFarmFrens__EligibilityExceeded();
+            }
+            whitelist[msg.sender].claimedAmount += mintAmount;
+        } else {
+            // public phase
+            if (mintAmount > maxPublicMints) {
+                revert BotFarmFrens__MaxPublicMintsExceeded();
+            }
+        }
+
         uint256 fee = price * mintAmount;
-        if (currency.balanceOf(msg.sender) < fee) {
-            revert BotFarmFrens__InsufficientCurrency();
-        }
-        currency.transferFrom(msg.sender, address(this), fee);
+        receiveFeeInternal(mintAmount);
+
         uint256[] memory mintedIds = new uint256[](mintAmount);
         for (uint256 i = 0; i < mintAmount; i++) {
             uint256 mintId = totalSupply();
@@ -65,10 +101,23 @@ contract BotFarmFrens is IBotFarmFrens, ERC721Enumerable, Ownable {
     }
 
     /// @inheritdoc IBotFarmFrens
+    function pauseSale() public override onlyOwner {
+        saleEnabled = false;
+        emit PauseSale();
+    }
+
+    /// @inheritdoc IBotFarmFrens
     function setBaseURI(string memory newBaseURI) public override onlyOwner {
         string memory oldBaseURI = baseURI;
         baseURI = newBaseURI;
         emit SetBaseURI(oldBaseURI, baseURI);
+    }
+
+    /// @inheritdoc IBotFarmFrens
+    function setMaxPublicMints(uint256 newMaxPublicMints) public override onlyOwner {
+        uint256 oldMaxPublicMints = maxPublicMints;
+        maxPublicMints = newMaxPublicMints;
+        emit SetMaxPublicMints(oldMaxPublicMints, maxPublicMints);
     }
 
     /// @inheritdoc IBotFarmFrens
@@ -79,9 +128,41 @@ contract BotFarmFrens is IBotFarmFrens, ERC721Enumerable, Ownable {
     }
 
     /// @inheritdoc IBotFarmFrens
+    function setWhitelist(address[] memory users, uint256 eligibleAmount) public override onlyOwner {
+        for (uint256 i = 0; i < users.length; i++) {
+            WhitelistElement memory elem;
+            elem.exists = true;
+            elem.eligibleAmount = eligibleAmount;
+            whitelist[users[i]] = elem;
+        }
+        emit SetWhitelist(users, eligibleAmount);
+    }
+
+    /// @inheritdoc IBotFarmFrens
+    function startSale() public override onlyOwner {
+        if (saleEnabled) {
+            revert BotFarmFrens__SaleAlreadyStarted();
+        }
+        saleStartTime = block.timestamp;
+        saleEnabled = true;
+        emit StartSale();
+    }
+
+    /// @inheritdoc IBotFarmFrens
     function withdraw(address recipient) public override onlyOwner {
         uint256 amount = currency.balanceOf(address(this));
         currency.transfer(recipient, amount);
         emit Withdraw(recipient, amount);
+    }
+
+    /// INTERNAL NON-CONSTANT FUNCTIONS ///
+
+    /// @notice Receive fee from user in currency units.
+    /// @param fee The fee amount to receive in currency units.
+    function receiveFeeInternal(uint256 fee) internal {
+        if (currency.balanceOf(msg.sender) < fee) {
+            revert BotFarmFrens__InsufficientCurrency();
+        }
+        currency.transferFrom(msg.sender, address(this), fee);
     }
 }
--- contracts/IBotFarmFrens.sol
+++ contracts/IBotFarmFrens.sol
@@ -14,15 +14,31 @@ interface IBotFarmFrens {
     /// @param fee The total mint fee paid in currency units.
     event MintBFF(uint256[] indexed ids, address indexed minter, uint256 fee);
 
+    /// @notice Emitted when sale is paused.
+    event PauseSale();
+
     /// @notice Emitted when base URI is set.
     /// @param oldBaseURI The old base URI.
     /// @param newBaseURI The new base URI.
     event SetBaseURI(string oldBaseURI, string newBaseURI);
 
+    /// @notice Emitted when maximum public mints is set.
+    /// @param oldMaxPublicMints The old maximum public mints.
+    /// @param newMaxPublicMints The new maximum public mints.
+    event SetMaxPublicMints(uint256 oldMaxPublicMints, uint256 newMaxPublicMints);
+
     /// @notice Emitted when mint price is set.
     /// @param oldPrice The old mint price.
     /// @param newPrice The new mint price.
     event SetPrice(uint256 oldPrice, uint256 newPrice);
+
+    /// @notice Emitted when a new subset of users is added to private sale whitelist.
+    /// @param users The user addresses.
+    /// @param eligibleAmount The amount of BFFs each user in provided list is eligible to mint.
+    event SetWhitelist(address[] indexed users, uint256 eligibleAmount);
+
+    /// @notice Emitted when sale is started.
+    event StartSale();
 
     /// @notice Emitted when withdrawing currency.
     /// @param recipient The recipient of currency withdrawn.
@@ -37,8 +53,28 @@ interface IBotFarmFrens {
     /// @notice The contract of token used for paying mint fees.
     function currency() external view returns (IERC20Metadata);
 
+    /// @notice The maximum number of BFFs that can be minted in one transaction at public sale phase.
+    function maxPublicMints() external view returns (uint256);
+
     /// @notice The mint price in currency tokens.
     function price() external view returns (uint256);
+
+    /// @notice The sale start timestamp.
+    /// Note: the sale starts with the private phase.
+    function saleStartTime() external view returns (uint256);
+
+    /// @notice The status of the sale.
+    function saleEnabled() external view returns (bool);
+
+    /// @notice The private sale phase whitelist.
+    function whitelist(address user)
+        external
+        view
+        returns (
+            bool exists,
+            uint256 eligibleAmount,
+            uint256 claimedAmount
+        );
 
     /// PUBLIC NON-CONSTANT FUNCTIONS ///
 
@@ -53,6 +89,14 @@ interface IBotFarmFrens {
     /// @param mintAmount The amount of BFFs to mint.
     function mintBFF(uint256 mintAmount) external;
 
+    /// @notice Pause the BFF sale.
+    ///
+    /// @dev Emits a {PauseSale} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    function pauseSale() external;
+
     /// @notice Set the base URI.
     ///
     /// @dev Emits a {SetBaseURI} event.
@@ -63,6 +107,16 @@ interface IBotFarmFrens {
     /// @param newBaseURI The new base URI.
     function setBaseURI(string memory newBaseURI) external;
 
+    /// @notice Set the maximum number of BFFs that can be minted in one transaction at public sale phase.
+    ///
+    /// @dev Emits a {SetMaxPublicMints} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param newMaxPublicMints The new maximum public mints.
+    function setMaxPublicMints(uint256 newMaxPublicMints) external;
+
     /// @notice Set the mint price.
     ///
     /// @dev Emits a {SetPrice} event.
@@ -72,6 +126,25 @@ interface IBotFarmFrens {
     ///
     /// @param newPrice The new BFF mint price.
     function setPrice(uint256 newPrice) external;
+
+    /// @notice Whitelist users to the private sale phase.
+    ///
+    /// @dev Emits a {SetWhitelist} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param users The user addresses to whitelist for private sale phase.
+    /// @param eligibleAmount The amount of BFFs each user in provided list is eligible to mint.
+    function setWhitelist(address[] memory users, uint256 eligibleAmount) external;
+
+    /// @notice Start the BFF sale.
+    ///
+    /// @dev Emits a {StartSale} event.
+    ///
+    /// Requirements:
+    /// - Can only be called by the owner.
+    function startSale() external;
 
     /// @notice Withdraw the currency contained in the contract.
     ///
