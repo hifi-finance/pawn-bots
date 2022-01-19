e046c1e7400cfd73f9df000495463ab3938d2f76
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2022-01-19 16:59:44 +0300

    feat: add `IPBTickets` and `PBTickets`

---
+++ contracts/IPBTickets.sol
@@ -0,0 +1,174 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+/// @title IPBTickets
+/// @author Hifi
+interface IPBTickets {
+    /// ENUMS ///
+
+    enum MintPhase {
+        PRIVATE,
+        PUBLIC
+    }
+
+    /// EVENTS ///
+
+    /// @notice Emitted when contract owner burns unsold tickets.
+    /// @param burnAmount The amount of unsold tickets burned.
+    event BurnUnsold(uint256 burnAmount);
+
+    /// @notice Emitted when a user mints new tickets.
+    /// @param minter The minter's account address.
+    /// @param mintAmount The amount of minted tickets.
+    /// @param price The mint price per ticket that was paid.
+    /// @param phase The mint phase.
+    event Mint(address indexed minter, uint256 mintAmount, uint256 price, MintPhase phase);
+
+    /// @notice Emitted when ticket sale is paused.
+    event PauseSale();
+
+    /// @notice Emitted when base URI is set.
+    /// @param baseURI The new base URI that was set.
+    event SetBaseURI(string baseURI);
+
+    /// @notice Emitted when maximum mints per transaction is set.
+    /// @param maxMintsPerTx The new maximum mints per transaction.
+    event SetMaxMintsPerTx(uint256 maxMintsPerTx);
+
+    /// @notice Emitted when mint price is set.
+    /// @param price The new mint price.
+    event SetPrice(uint256 price);
+
+    /// @notice Emitted when sale is started.
+    event StartSale();
+
+    /// @notice Emitted when funds are withdrawn from the contract.
+    /// @param recipient The recipient of funds withdrawn.
+    /// @param amount The amount of funds withdrawn.
+    event Withdraw(address indexed recipient, uint256 amount);
+
+    /// PUBLIC CONSTANT FUNCTIONS ///
+
+    /// @notice Whether an account is whitelisted for the private phase of the sale or not.
+    /// @param account The account address to check.
+    /// @param merkleProof The merkle proof of the account being whitelisted.
+    function isWhitelisted(address account, bytes32[] calldata merkleProof) external view returns (bool);
+
+    /// @notice The maximum amount of tickets that can ever exist.
+    function maxElements() external view returns (uint256);
+
+    /// @notice The maximum amount of tickets per user per transaction that can be minted during the public phase.
+    function maxMintsPerTx() external view returns (uint256);
+
+    /// @notice The mint price in ETH.
+    function price() external view returns (uint256);
+
+    /// @notice The status of the sale.
+    function saleIsActive() external view returns (bool);
+
+    /// @notice The sale start timestamp.
+    /// Note: the sale starts with the private phase, which lasts 24 hrs.
+    function saleStartTime() external view returns (uint256);
+
+    /// PUBLIC NON-CONSTANT FUNCTIONS ///
+
+    /// @notice Burn unsold tickets after the sale.
+    ///
+    /// @dev Emits a {BurnUnsold} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    /// - Can only be called when sale is paused.
+    /// - `burnAmount` cannot exceed `maxElements` - `totalSupply()`.
+    ///
+    /// @param burnAmount The amount of tickets to burn.
+    function burnUnsold(uint256 burnAmount) external;
+
+    /// @notice Mint new tickets in exchange for ETH during the private phase.
+    ///
+    /// @dev Emits a {Mint} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called when sale is active.
+    /// - Can only be called within the first 24 hours of the sale.
+    /// - Caller must be whitelisted for the private phase.
+    /// - `mintAmount` cannot exceed `maxMintsPerTx`.
+    /// - `mintAmount` cannot exceed `maxElements` minus `totalSupply()`.
+    /// - Can only be called when caller has placed at least `price * mintAmount` ETH as the transaction value.
+    ///
+    /// @param mintAmount The amount of tickets to mint.
+    /// @param merkleProof The merkle proof of caller being whitelisted.
+    function mintPrivate(uint256 mintAmount, bytes32[] calldata merkleProof) external payable;
+
+    /// @notice Mint new tickets in exchange for ETH during the public phase.
+    ///
+    /// @dev Emits a {Mint} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called when sale is active.
+    /// - Can only be called after the first 24 hours of the sale.
+    /// - `mintAmount` cannot exceed `maxMintsPerTx`.
+    /// - `mintAmount` cannot exceed `maxElements` minus `totalSupply()`.
+    /// - Can only be called when caller has placed at least `price * mintAmount` ETH as the transaction value.
+    ///
+    /// @param mintAmount The amount of tickets to mint.
+    function mintPublic(uint256 mintAmount) external payable;
+
+    /// @notice Pause the sale.
+    ///
+    /// @dev Emits a {PauseSale} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    /// - Sale must be active.
+    function pauseSale() external;
+
+    /// @notice Set the base URI.
+    ///
+    /// @dev Emits a {SetBaseURI} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param newBaseURI The new base URI.
+    function setBaseURI(string memory newBaseURI) external;
+
+    /// @notice Set the maximum amount of tickets that can be minted by any user in one transaction.
+    ///
+    /// @dev Emits a {SetMaxMintsPerTx} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param newMaxMintsPerTx The new maximum mints per transaction.
+    function setMaxMintsPerTx(uint256 newMaxMintsPerTx) external;
+
+    /// @notice Set the mint price.
+    ///
+    /// @dev Emits a {SetPrice} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param newPrice The new ticket mint price.
+    function setPrice(uint256 newPrice) external;
+
+    /// @notice Start the ticket sale.
+    ///
+    /// @dev Emits a {StartSale} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    /// - Sale must not already be active.
+    function startSale() external;
+
+    /// @notice Withdraw the accumulated funds in the contract.
+    ///
+    /// @dev Emits a {Withdraw} event.
+    ///
+    /// @dev Requirements:
+    /// - Can only be called by the owner.
+    ///
+    /// @param recipient The recipient of funds withdrawn.
+    function withdraw(address recipient) external;
+}
---
+++ contracts/PBTickets.sol
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "hardhat/console.sol";
+import "./IPBTickets.sol";
+
+// TODO: rename `sale` to `phase`
+
+error PBTickets__InsufficientFundsSent();
+error PBTickets__MaxElementsExceeded();
+error PBTickets__MaxMintsPerTxExceeded();
+error PBTickets__NonexistentToken();
+error PBTickets__NotWhitelistedForPrivatePhase();
+error PBTickets__PrivatePhaseIsOver();
+error PBTickets__PublicPhaseNotYetStarted();
+error PBTickets__SaleIsActive();
+error PBTickets__SaleIsNotActive();
+
+/// @title PBTickets
+/// @author Hifi
+/// @notice Manages the mint and distribution of NFTs.
+contract PBTickets is IPBTickets, ERC721Enumerable, Ownable, ReentrancyGuard {
+    using Strings for uint256;
+
+    /// PUBLIC STORAGE ///
+
+    /// @dev The theoretical collection size.
+    uint256 public constant COLLECTION_SIZE = 10_000;
+
+    /// @dev The private sale duration from the sale start timestamp.
+    uint256 public constant PRIVATE_SALE_DURATION = 24 hours;
+
+    /// @inheritdoc IPBTickets
+    uint256 public override maxElements = COLLECTION_SIZE;
+
+    /// @inheritdoc IPBTickets
+    uint256 public override maxMintsPerTx;
+
+    /// @inheritdoc IPBTickets
+    uint256 public override price;
+
+    /// @inheritdoc IPBTickets
+    bool public override saleIsActive;
+
+    /// @inheritdoc IPBTickets
+    uint256 public override saleStartTime;
+
+    /// INTERNAL STORAGE ///
+
+    /// @dev The base token URI.
+    string internal baseURI;
+
+    /// @dev The whitelist merkle root.
+    bytes32 internal merkleRoot;
+
+    constructor(bytes32 merkleRoot_) ERC721("Pawn Bots Mint Tickets", "PBTKT") {
+        merkleRoot = merkleRoot_;
+    }
+
+    /// PUBLIC CONSTANT FUNCTIONS ///
+
+    /// @inheritdoc IPBTickets
+    function isWhitelisted(address account, bytes32[] calldata merkleProof) public view override returns (bool) {
+        return MerkleProof.verify(merkleProof, merkleRoot, keccak256(abi.encodePacked(account)));
+    }
+
+    /// @dev See {ERC721-tokenURI}.
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        if (!_exists(tokenId)) {
+            revert PBTickets__NonexistentToken();
+        }
+        string memory bURI = _baseURI();
+        return bytes(bURI).length > 0 ? string(abi.encodePacked(bURI, "ticket", ".json")) : "";
+    }
+
+    /// PUBLIC NON-CONSTANT FUNCTIONS ///
+
+    /// @inheritdoc IPBTickets
+    function burnUnsold(uint256 burnAmount) public override onlyOwner {
+        if (saleIsActive) {
+            revert PBTickets__SaleIsActive();
+        }
+        if (burnAmount + totalSupply() > maxElements) {
+            revert PBTickets__MaxElementsExceeded();
+        }
+
+        maxElements -= burnAmount;
+        emit BurnUnsold(burnAmount);
+    }
+
+    /// @inheritdoc IPBTickets
+    function mintPrivate(uint256 mintAmount, bytes32[] calldata merkleProof) public payable override nonReentrant {
+        if (!saleIsActive) {
+            revert PBTickets__SaleIsNotActive();
+        }
+        if (block.timestamp > saleStartTime + PRIVATE_SALE_DURATION) {
+            revert PBTickets__PrivatePhaseIsOver();
+        }
+        if (!isWhitelisted(msg.sender, merkleProof)) {
+            revert PBTickets__NotWhitelistedForPrivatePhase();
+        }
+
+        mintInternal(mintAmount);
+        emit Mint(msg.sender, mintAmount, price, MintPhase.PRIVATE);
+    }
+
+    /// @inheritdoc IPBTickets
+    function mintPublic(uint256 mintAmount) public payable override nonReentrant {
+        if (!saleIsActive) {
+            revert PBTickets__SaleIsNotActive();
+        }
+        if (block.timestamp <= saleStartTime + PRIVATE_SALE_DURATION) {
+            revert PBTickets__PublicPhaseNotYetStarted();
+        }
+
+        mintInternal(mintAmount);
+        emit Mint(msg.sender, mintAmount, price, MintPhase.PUBLIC);
+    }
+
+    /// @inheritdoc IPBTickets
+    function pauseSale() public override onlyOwner {
+        if (!saleIsActive) {
+            revert PBTickets__SaleIsNotActive();
+        }
+
+        saleIsActive = false;
+        emit PauseSale();
+    }
+
+    /// @inheritdoc IPBTickets
+    function setBaseURI(string memory newBaseURI) public override onlyOwner {
+        baseURI = newBaseURI;
+        emit SetBaseURI(newBaseURI);
+    }
+
+    /// @inheritdoc IPBTickets
+    function setMaxMintsPerTx(uint256 newMaxMintsPerTx) public override onlyOwner {
+        maxMintsPerTx = newMaxMintsPerTx;
+        emit SetMaxMintsPerTx(maxMintsPerTx);
+    }
+
+    /// @inheritdoc IPBTickets
+    function setPrice(uint256 newPrice) public override onlyOwner {
+        price = newPrice;
+        emit SetPrice(price);
+    }
+
+    /// @inheritdoc IPBTickets
+    function startSale() public override onlyOwner {
+        if (saleIsActive) {
+            revert PBTickets__SaleIsActive();
+        }
+        if (saleStartTime == 0) {
+            saleStartTime = block.timestamp;
+        }
+        saleIsActive = true;
+        emit StartSale();
+    }
+
+    /// @inheritdoc IPBTickets
+    function withdraw(address recipient) public override onlyOwner {
+        uint256 amount = address(this).balance;
+        Address.sendValue(payable(recipient), amount);
+        emit Withdraw(recipient, amount);
+    }
+
+    /// INTERNAL CONSTANT FUNCTIONS ///
+
+    /// @dev See {ERC721-_baseURI}.
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    /// INTERNAL NON-CONSTANT FUNCTIONS ///
+
+    /// @dev Mint NFTs in exchange for fees.
+    function mintInternal(uint256 mintAmount) internal {
+        if (mintAmount > maxMintsPerTx) {
+            revert PBTickets__MaxMintsPerTxExceeded();
+        }
+        uint256 totalSupply = totalSupply();
+        if (mintAmount + totalSupply > maxElements) {
+            revert PBTickets__MaxElementsExceeded();
+        }
+
+        uint256 mintCost = price * mintAmount;
+        if (msg.value < mintCost) {
+            revert PBTickets__InsufficientFundsSent();
+        }
+
+        for (uint256 i = 0; i < mintAmount; i++) {
+            uint256 mintId = totalSupply + i;
+            _safeMint(msg.sender, mintId);
+        }
+
+        if (msg.value > mintCost) {
+            Address.sendValue(payable(msg.sender), msg.value - mintCost);
+        }
+    }
+}
